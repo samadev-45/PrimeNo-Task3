7b40dd19413598fa604a8a8b3b701af1813d9cb8
samadev-45 <samadev45@gmail.com>
2025-08-05 21:16:26 +0530

    Add project files.

---
+++ PrimeNo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PrimeNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            int number=3;
+            if (number <= 1)
+            {
+                Console.WriteLine("Its not a prime");
+                return;
+            }
+            for(int i=2; i <= number - 1; i++)
+            {
+                if(number%i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+                
+            }
+            if(isPrime == true)
+            {
+                Console.WriteLine("its prime no");
+            }
+            else
+            {
+                Console.WriteLine("Its not prime no");
+            }
+        }
+    }
+}
